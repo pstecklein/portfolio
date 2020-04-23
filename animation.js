58e4468c1c16b27510fa920972042ddf09a7754a
Preston <psteckleintt@gmail.com>
2020-04-23 16:56:47 -0400

    added js and responsive css files

---
+++ animation.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    "use strict";
+    $('.qLink').hide().fadeIn(4000);
+    $('.picture').hide().fadeIn(2000);
+    $('.portfolio').hide().slideDown(3000);
+    $('.projects').hide().fadeIn(3000);
+});
+
+
+$(document).on('scroll', function () {
+    "use strict";
+    var pageTop = $(document).scrollTop(),
+        pageBottom = pageTop + $(window).height(),
+        tags = $('.reveal'),
+        i = 0;
+    
+    for (i; i < tags.length; i += 1) {
+        var tag = tags[i];
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass('visible');
+        } else {
+            $(tag).removeClass('.visible');
+        }
+    }
+});
