83283eb9559735e0592db3d8c759af99f19b1459
Preston <psteckleintt@gmail.com>
2020-06-14 14:35:56 -0400

    Add files via upload

--- animation.js
+++ animation.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     "use strict";
     $('.qLink').hide().fadeIn(4000);
-    $('.picture').hide().fadeIn(2000);
+    $('.hello').hide().fadeIn(4000);
+    $('.about').hide().delay(1000).fadeIn(2000);
     $('.portfolio').hide().slideDown(3000);
     $('.projects').hide().fadeIn(3000);
 });
@@ -13,7 +14,7 @@ $(document).on('scroll', function () {
         pageBottom = pageTop + $(window).height(),
         tags = $('.reveal'),
         i = 0;
-    
+
     for (i; i < tags.length; i += 1) {
         var tag = tags[i];
         if ($(tag).position().top < pageBottom) {
